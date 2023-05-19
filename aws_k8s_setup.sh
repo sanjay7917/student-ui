cfab7c5b38eb551d2f6ac7878476608eee3bf2c0
Sanju <killu@Sanjus-MacBook-Air.local>
2023-05-20 00:14:56 +0530

    mistake

---
+++ aws_k8s_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt update -y
+apt install unzip -y && apt install curl -y
+
+#AWSCLI INSTALLATION
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
+#EKSCTL INSTALLATION
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv kubectl /usr/local/bin/
+
+#EKSCTL INSTALLATION
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+mv /tmp/eksctl /usr/local/bin
